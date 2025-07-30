b5fd17ed4ed1c1336aef7d4a03440e2c1e2a89de
Harsh Patel <harshpatel79871@gmail.com>
2025-07-30 22:30:08 +0530

    eslint type check removed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,13 @@ const eslintConfig = [
 
   {
     rules: {
-      "@typescript-eslint/ban-ts-comment": "off"
+      // turn off common type-checking rules
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/ban-types": "off",
     }
   }
 ];
